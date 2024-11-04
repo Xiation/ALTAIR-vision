82362c56f7f86b42f06385660a40ee7603f9aeca
Xiation <abyanraditya.rakapasha@gmail.com>
2024-11-04 21:31:24 +0700

    new

--- src/fieldDetection.py
+++ src/fieldDetection.py
@@ -1,5 +1,15 @@
-import numpy 
+import numpy as np
 import cv2
+
+# Define color thresholds in HSV color space for field and lines
+
+# green field
+green_lower = np.array([36,100,100], dtype=np.uint8)
+green_upper = np.array([86,255,255], dtype=np.uint8)
+
+# white line
+white_lower = np.array([0,0,200], dtype=np.uint8)
+white_upper = np.array([180,20,255], dtype=np.uint8)
 
 def removeBackground():
     ''''''''
