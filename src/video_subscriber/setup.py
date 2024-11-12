5ad572c6937f2852cd8b8d8001f5dc53bbca6250
Xiation <abyanraditya.rakapasha@gmail.com>
2024-11-12 21:42:51 +0700

    new

--- src/video_subscriber/setup.py
+++ src/video_subscriber/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'gui_subscriber = video_subscriber.gui_subscriber:main'
+        'gui_subscriber = video_subscriber.vid_subscriber:main'
         ],
     },
 )
