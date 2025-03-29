d7795d220e96c413d2950115bdc717fa2c066567
Xiation <abyanraditya.rkpash@gmail.com>
2025-03-29 13:21:54 +0700

    update ball detector

---
+++ src/ball_detect/include/ball_detect/ball_detect.hpp
@@ -0,0 +1,15 @@
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <numeric>
+
+#include <utility>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <cv_bridge/cv_bridge.h>
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "std_msgs/msg/string.hpp"
--- src/ball_detect/src/ball_detect.cpp
+++ src/ball_detect/src/ball_detect.cpp
@@ -1,15 +1,1 @@
-#include <opencv2/opencv.hpp>
-#include <vector>
-#include <numeric>
-
-#include <utility>
-#include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.h>
-
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
-
-#include "std_msgs/msg/string.hpp"
+#include <include/ball_detect/ball_detect.hpp>
