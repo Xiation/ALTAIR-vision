fea04a19e71fc2d390f3c2bcb30b21437f704b80
Xiation <abyanraditya.rkpash@gmail.com>
2025-03-29 16:32:07 +0700

    brooo helpp

--- install/temp_ball_detector/share/temp_ball_detector/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(temp_ball_detector_IDL_FILES "msg/BallDistance.idl")
-set(temp_ball_detector_INTERFACE_FILES "msg/BallDistance.msg")
